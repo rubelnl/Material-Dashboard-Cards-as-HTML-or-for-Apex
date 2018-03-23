017d4422fe68744ec694b424b81e96acdba74d3c
Ronny Weiss <30749177+RonnyWeiss@users.noreply.github.com>
2018-03-23 19:13:33 +0100

    improving util.escapeHTML

--- js/script.js
+++ js/script.js
@@ -1,22 +1,33 @@
 var materialCards = (function () {
     "use strict";
-    var scriptVersion = "1.2.2";
+    var scriptVersion = "1.2.3";
     var util = {
         version: "1.0",
         escapeHTML: function (str) {
-            if (str && str.length > 0) {
+            if (str === null) {
+                return null;
+            }
+            if (typeof str === "undefined") {
+                return;
+            }
+            if (typeof str === "object") {
                 try {
-                    return apex.util.escapeHTML(str.toString());
+                    str = JSON.stringify(str);
                 } catch (e) {
-                    return str
-                        .toString()
-                        .replace(/&/g, "&amp;")
-                        .replace(/</g, "&lt;")
-                        .replace(/>/g, "&gt;")
-                        .replace(/"/g, "&quot;")
-                        .replace(/'/g, "&#x27;")
-                        .replace(/\//g, "&#x2F;");
+                    /*do nothing */
                 }
+            }
+            try {
+                return apex.util.escapeHTML(String(str));
+            } catch (e) {
+                str = String(str);
+                return str
+                    .replace(/&/g, "&amp;")
+                    .replace(/</g, "&lt;")
+                    .replace(/>/g, "&gt;")
+                    .replace(/"/g, "&quot;")
+                    .replace(/'/g, "&#x27;")
+                    .replace(/\//g, "&#x2F;");
             }
         },
         loader: {
